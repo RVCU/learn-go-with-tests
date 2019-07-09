66880910f4264b45d2bb3bba1cc443c7d9b7d476
RVCU <reillylefort20@gmail.com>
2019-07-09 23:32:03 +0000

    ouu benchmarks

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -2,6 +2,11 @@ package iteration
 
 import "testing"
 
+func BenchmarkRepeat(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Repeat("a")
+    }
+}
 func TestRepeat(t *testing.T) {
     repeated := Repeat("racecar")
     expected := "racecarracecarracecarracecarracecar"
