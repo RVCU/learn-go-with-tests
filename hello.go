05d293f5ba90776a951f350552eaf4a85364a9d0
RVCU <reillylefort20@gmail.com>
2019-07-09 22:55:51 +0000

    adding languages

--- hello.go
+++ hello.go
@@ -2,12 +2,27 @@ package main
 
 import "fmt"
 
-const helloPrefix = "Hello "
+const en_hello_prefix = "Hello "
+const spanish = "Spanish"
+const sp_hello_prefix = "Hola "
+const french = "French"
+const fr_hello_prefix = "Bonjour "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
     if name == "" { name = "World" }
-    return helloPrefix + name
+    return greetingPrefix(language) + name
+}
+func greetingPrefix(language string) (prefix string) {
+    switch language {
+        case french:
+            prefix = fr_hello_prefix
+        case spanish:
+            prefix = sp_hello_prefix
+        default:
+            prefix = en_hello_prefix
+    }
+    return
 }
 func main() {
-    fmt.Println(Hello("Chris"))
+    fmt.Println(Hello("Chris", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -4,18 +4,23 @@ import "testing"
 
 func TestHello(t *testing.T) {
     assertCorrectMessage := func(t *testing.T, got, want string) {
-        t.Helper()
+            t.Helper() //this tells the testing framework this func is a helper
         if got != want {
             t.Errorf("got '%s' want '%s'", got, want)
         }
     }
+    t.Run("In Spanish", func(t *testing.T) {
+        got := Hello("Elodie", "Spanish")
+        want := "Hola Elodie"
+        assertCorrectMessage(t, got, want)
+    })
     t.Run("saying hello to people", func(t *testing.T) {
-        got := Hello("Chris")
+        got := Hello("Chris", "")
         want := "Hello Chris"
         assertCorrectMessage(t, got, want)
     })
     t.Run("saying hello world if no nname is provided", func(t *testing.T) {
-        got := Hello("")
+        got := Hello("", "")
         want := "Hello World"
         assertCorrectMessage(t, got, want)
     })
