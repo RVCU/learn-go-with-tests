55312ffe1420b8697f6a809b723a76bc0667649a
RVCU <reillylefort20@gmail.com>
2019-07-09 23:43:16 +0000

    adding in a example and refactor to take repeat int

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,23 +1,29 @@
 package iteration
 
+import "fmt"
 import "testing"
 
 func BenchmarkRepeat(b *testing.B) {
     for i := 0; i < b.N; i++ { //https://golang.org/pkg/testing/#hdr-Benchmarks
-        Repeat("a")
+        Repeat("a",1000000000)
     }
 }
+func ExampleRepeat() {
+    returnString := Repeat("b",3)
+    fmt.Println(returnString)
+    //Output: bbb
+}
 func TestRepeat(t *testing.T) {
-    repeated := Repeat("racecar")
+    repeated := Repeat("racecar",5)
     expected := "racecarracecarracecarracecarracecar"
     if repeated != expected {
         t.Errorf("expected, '%s' but got '%s'", expected, repeated)
     }
 }
 
-func Repeat(daString string) string {
+func Repeat(daString string, repeatNumber int) string {
     var repeatedString string
-    for i := 0; i < 5; i++ {
+    for i := 0; i < repeatNumber; i++ {
        repeatedString += daString
     }
     return repeatedString
